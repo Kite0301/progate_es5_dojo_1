cd4f28944634e1f3874e44c8b781a334b9412cc3
Kite0301 <kaito.titech@gmail.com>
2017-09-23 16:53:15 +0900

    Add page5 answer

---
+++ page5.js
@@ -0,0 +1,14 @@
+var height = 1.7;
+console.log("height: " + height);
+var weight = 60;
+console.log("weight: " + weight);
+
+// この下にコードを書いてください
+
+// Instruction 1
+var bmi = weight / height / height;
+console.log("BMIは" + bmi + "です");
+
+// Instruction 2
+var idealWeight = height * height * 22;
+console.log("適正体重は" + idealWeight + "kgです");
