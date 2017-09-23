f2a7c152c3f1dd41ee0fa265a3b21f46ae2e3bd4
Kite0301 <kaito.titech@gmail.com>
2017-09-23 17:48:56 +0900

    Add page10 answer

---
+++ page10.js
@@ -0,0 +1,17 @@
+var user = {
+  name: '太郎',
+  age: 21,
+  nationality: '日本',
+}
+
+// この下にコードを書いてください
+
+console.log("名前：" + user.name);
+console.log("年齢：" + user.age);
+console.log("国籍：" + user.nationality);
+
+// 別解
+
+console.log("名前：" + user["name"]);
+console.log("年齢：" + user["age"]);
+console.log("国籍：" + user["nationality"]);
