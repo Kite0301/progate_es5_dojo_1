793fe759b247c0a0d5be6e22b653b109cf38f7c0
Kite0301 <kaito.titech@gmail.com>
2017-09-23 16:27:36 +0900

    Add page2 answer

---
+++ page2.js
@@ -0,0 +1,9 @@
+var firstName = "Tom";
+console.log("firstName: " + firstName);
+var lastName = "Brown";
+console.log("lastName: " + lastName);
+
+// この下にコードを書いてください
+
+// Instruction 1
+console.log("Hello, " + firstName + lastName);
