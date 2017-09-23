f6d3b0c5ec2c3fc8c500ee5c2a68e1dd81d31862
Kite0301 <kaito.titech@gmail.com>
2017-09-23 17:59:55 +0900

    Update page2 answer

--- page2.js
+++ page2.js
@@ -1,9 +1,9 @@
-var firstName = "Tom";
+var firstName = "山田";
 console.log("firstName: " + firstName);
-var lastName = "Brown";
+var lastName = "太郎";
 console.log("lastName: " + lastName);
 
 // この下にコードを書いてください
 
 // Instruction 1
-console.log("Hello, " + firstName + lastName);
+console.log("こんにちは, " + firstName + lastName);
